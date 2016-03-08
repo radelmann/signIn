6c77c455dc9d3b58eb63eabd832803139806616c
RobAdelmann <robadelmann@gmail.com>
2016-03-07 21:13:34 -0800

    updated express session config settings

--- config/env.js
+++ config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'session-secret' : process.env.SIGNIN_PROD_SESSION_SECRET || 'dev-session-secret',
+    'session_secret' : process.env.SIGNIN_PROD_SESSION_SECRET || 'dev-session-secret',
     'port' : process.env.SIGNIN_PROD_PORT || 3000,
-    'db-url' : process.env.SIGNIN_PROD_DB_URL || 'mongodb://localhost/signin' 
+    'db_url' : process.env.SIGNIN_PROD_DB_URL || 'mongodb://localhost/signin' 
 };
--- server.js
+++ server.js
@@ -11,28 +11,30 @@ var session = require('express-session');
 
 var env = require('./config/env.js');
 
-mongoose.connect(env.db-url);
+mongoose.connect(env.db_url);
 
-app.use(morgan('dev')); 
-app.use(cookieParser()); 
+app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 app.use(session({
-  secret: env.session-secret
-})); 
+  secret: env.session_secret,
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use(passport.initialize());
-app.use(passport.session()); 
-app.use(flash()); 
+app.use(passport.session());
+app.use(flash());
 
-require('./app/routes.js')(app, passport); 
+require('./app/routes.js')(app, passport);
 
-app.listen(env.port, function() {
-  console.log('listening on port: ' env.port);
+app.listen(env.port, function () {
+  console.log('listening on port: ' + env.port);
 });
